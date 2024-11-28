cc4609f8c8fdc16682bdc410ae8e96d6d579a884
Bobb-93 <borisiakimov@gmail.com>
2024-11-28 18:41:58 +0200

    Quiz Game: Created files; first steps

---
+++ QuizGame/main.js
@@ -0,0 +1,20 @@
+//Array of questions
+let questions = [ "What is the capital of France?", 
+    "Which language is used for Front-end Web development?",
+    "What does CSS stand for?"
+];
+
+//Array of options for each question
+let options = [
+    ["Berlin", "Madrid", "Paris", "Lisbon"],
+    ["Python", "JavaScript", "C++", "Java"],
+    ["Cascading Style Sheets", "Creative Style System", "Computer Style Sheets", "Colorful Style Sheets"]
+];
+
+//Array of correct options
+let answers = [
+    "Paris",
+    "JavaScript",
+    "Cascading Style Sheets"
+];
+
