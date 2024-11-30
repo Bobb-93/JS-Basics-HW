11a2526defe7dc34933d43d92703f5c844f9cc94
Bobb-93 <borisiakimov@gmail.com>
2024-11-30 09:10:56 +0200

    SOLVED: twoDimensionalArray

---
+++ HW2/twoDimensionalArray.js
@@ -0,0 +1,19 @@
+let fruitsData = [
+  ["apple", "orange", "cherry"],
+  [1.2, 2.0, 3.5],
+];
+
+// TASK: log in console each fruit and its price
+
+// YOUR CODE HERE
+
+for (let j = 0; j < fruitsData[0].length; j++) {
+
+  console.log(`${fruitsData[0][j]} = ${(Math.round(fruitsData[1][j] * 100) / 100).toFixed(1)}`);
+
+}
+
+// EXPECTED OUTPUT:
+// apple = 1.2,
+// orange = 2.0,
+// cherry = 3.5
