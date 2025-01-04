a83836868792d001ec81009f68712c32fb520072
Bobb-93 <borisiakimov@gmail.com>
2025-01-04 11:55:16 +0200

    Solved in HW7 - JS and CSS -> querySelector and Styling with JS

--- HW7 - JS and CSS/querySelector and styling with JS/main.js
+++ HW7 - JS and CSS/querySelector and styling with JS/main.js
@@ -3,6 +3,17 @@ console.dir(li3Node);
 li3Node.style.color = "red";
 
 // ************************************
-// Задача: поставете един бутон в HTML-a и променете програмата, така че li елемента (<li>Am I green or am I red?</li>) да променя цвета на шрифта си от червен към зелен при всяко натискане на бутона
+// Задача: поставете един бутон в HTML-a и променете програмата, така че li елемента (<li>Am I green or am I red?</li>) да променя цвета на шрифта си от 
+// червен към зелен при всяко натискане на бутона
 // ************************************
+const changeLiColor = document.getElementById("change-li-color");
 
+changeLiColor.addEventListener("click", function(){
+    
+    if(li3Node.style.color === "red"){
+        li3Node.style.color = "green";
+    } else{
+        li3Node.style.color = "red";
+    }
+
+});
