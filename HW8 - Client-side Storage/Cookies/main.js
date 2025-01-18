7fd851eb65c8d6e6b936bff0e0459693fb962b0c
Bobb-93 <borisiakimov@gmail.com>
2025-01-18 11:31:29 +0200

    create main.js in Cookies and LocalStorage folders

---
+++ HW8 - Client-side Storage/Cookies/main.js
@@ -0,0 +1,7 @@
+/*
+    You have to store the color selected by the user, so that next time when she enters the site, 
+    her chosen colour would be set as page background colour.
+    
+    Implement the task with cookies.
+
+*/
