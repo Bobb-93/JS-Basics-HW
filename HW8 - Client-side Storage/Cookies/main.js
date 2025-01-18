46ae28475885c8efddf9084a381d7fbc9f0698bd
Bobb-93 <borisiakimov@gmail.com>
2025-01-18 12:09:36 +0200

    HW8: solve with cookies

--- HW8 - Client-side Storage/Cookies/main.js
+++ HW8 - Client-side Storage/Cookies/main.js
@@ -5,3 +5,31 @@
     Implement the task with cookies.
 
 */
+
+const bgChoiceSelect = document.querySelector(".bgChoice select");
+const bgChoiceButton = document.querySelector(".bgChoice button");
+
+bgChoiceButton.addEventListener("click", function(){
+    let bgChoiceValue = bgChoiceSelect.value;
+    //console.log(bgChoiceValue);
+
+    //Set the current background color of the body
+    document.body.style.backgroundColor = bgChoiceValue;
+
+    //Set the cookie value
+    document.cookie = `bgColor=${bgChoiceValue}`;
+
+});
+
+let backgroundColorCookie = document.cookie;
+console.log(backgroundColorCookie);
+
+//Get the cookie value
+let cookieColor = backgroundColorCookie.substring(8);
+
+if(cookieColor){
+    document.body.style.backgroundColor = cookieColor;
+    bgChoiceSelect.value = cookieColor;
+} else{
+    document.body.style.backgroundColor = "#FFFFFF";
+}
