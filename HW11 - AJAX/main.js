fa583ac64acb0a10897f0efc0a9c2c6f07efd175
Bobb-93 <borisiakimov@gmail.com>
2025-02-01 15:38:10 +0200

    HW11 - AJAX folder -> add explanatory comment

--- HW11 - AJAX/main.js
+++ HW11 - AJAX/main.js
@@ -2,6 +2,7 @@ const url = "https://api.chucknorris.io/jokes/random "; // work as intended
 
 // const url = "https://api.chucknorris.io/jokses/random "; // undefined; 
 // the whole object is: {timestamp: '2025-02-01T13:32:56.685Z', status: 404, error: 'Not Found', path: '/jokses/random'}
+//joke does not have property value
 
 // const url = "https://api.chucknsorris.io/jokes/rasndom "; // Error: TypeError: Failed to fetch
 //we don't get any object
