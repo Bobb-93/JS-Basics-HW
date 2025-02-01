182632db427f2394163de11d5c054a34024027f5
Bobb-93 <borisiakimov@gmail.com>
2025-02-01 15:36:30 +0200

    HW11 - AJAX folder -> check case when we get undefined and case when we get error

--- HW11 - AJAX/main.js
+++ HW11 - AJAX/main.js
@@ -1,4 +1,11 @@
-const url = "https://api.chucknorris.io/jokes/random ";
+const url = "https://api.chucknorris.io/jokes/random "; // work as intended
+
+// const url = "https://api.chucknorris.io/jokses/random "; // undefined; 
+// the whole object is: {timestamp: '2025-02-01T13:32:56.685Z', status: 404, error: 'Not Found', path: '/jokses/random'}
+
+// const url = "https://api.chucknsorris.io/jokes/rasndom "; // Error: TypeError: Failed to fetch
+//we don't get any object
+
 
 const dom = {
     showJokeButtton: document.getElementById("show-joke-buttton"),
@@ -8,5 +15,7 @@ const dom = {
 dom.showJokeButtton.addEventListener("click", function () {
     fetch(url)
         .then(response => response.json())
-        .then(joke => dom.jokeText.innerText = joke.value);
+        .then(joke => dom.jokeText.innerText = joke.value)
+        // .then(joke => console.log(joke)) // testing
+        .catch(error => dom.jokeText.innerText = `Error: ${error}`);;
 });
